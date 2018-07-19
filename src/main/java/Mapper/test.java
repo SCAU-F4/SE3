89f14532b2053a46cdc595a0a0b47a39bc46deb4
TongTong <2285077293@qq.com>
2018-07-19 15:41:14 +0800

    hi

--- src/main/java/Mapper/test.java
+++
@@ -1,9 +0,0 @@
-package Mapper;
-public class test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-          System.out.println("Hello");
-	}
-
-}
--- src/test/java/Mapper/test1.java
+++ src/test/java/Mapper/test1.java
@@ -1,9 +1,25 @@
 package Mapper;
+
+import java.io.IOException;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import pojo.Manager;
+
 public class test1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-      System.out.println("jjjjj");
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("SpringConf.xml");
+		ManagerDao managerMapper=ctx.getBean(ManagerDao.class);
+		Manager manager=new Manager();
+		manager.setManagerId(5);
+		manager.setManagerName("DickMing");
+		manager.setManagerPwd("12345678");
+		managerMapper.insert(manager);
+		int id=manager.getManagerId();
+		System.out.println(managerMapper.findById(id));
 	}
 
 }
