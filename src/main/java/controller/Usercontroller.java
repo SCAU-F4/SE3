ef256b6791bdc8ed307d5cafb9a26668647955d9
Alex <1515186599@qq.com>
2018-07-24 00:59:37 +0800

    mybatis重构

---
+++ src/main/java/controller/Usercontroller.java
@@ -0,0 +1,33 @@
+package controller;
+
+import java.sql.Timestamp;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mysql.fabric.xmlrpc.base.Data;
+
+import bean.Customer;
+
+@Controller
+@RequestMapping(value="user")
+public class Usercontroller {
+	  @RequestMapping(value="signin")
+	  public String signin(){
+		  return "signin";
+	  }
+	  @RequestMapping(value="signup",method=RequestMethod.GET)
+	  public String signup(){
+		  return "signup";
+	  }
+	  @RequestMapping(value="signup",method=RequestMethod.POST)
+	  public String signupcheck(Customer customer){
+		  //检验数据 AOP
+		  //成功,存进数据库
+		  customer.setCustomerRegDate(new Timestamp(System.currentTimeMillis()));
+		  return "redirect:/";
+	  }
+}
