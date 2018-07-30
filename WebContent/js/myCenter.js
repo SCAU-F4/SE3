18f0d3a2a29561d135c4fc96f97ce1325767b1c2
kimoji <376899065@qq.com>
2018-07-30 10:33:43 +0800

    shi

--- WebContent/js/myCenter.js
+++ WebContent/js/myCenter.js
@@ -1,25 +1,33 @@
 $(document).ready(function () {
 	 var $modify;
     $(".Order-top>ul>li").click(function () {
-        $(".Order-list .showli").removeClass("showli");
+        $(".Order-list .showli").removeClass("showli").hide();
         $(this).addClass("active").siblings().removeClass("active");
         var select=$(this).attr("data-select");console.log(select);
         if (select!=0) {
-            $("span.deal-state[data-state=" + select + "]").parents(".panel").addClass("showli").fadeIn("fast").siblings(":not(.showli)").fadeOut("fast");
+        	console.log($("span.deal-state[data-state=" + select + "]").text());
+        	$("span.deal-state[data-state=" + select + "]").parents(".panel").addClass("showli").fadeIn().siblings(":not(.showli)").fadeOut();;
         }
         else
         {
-            $(".panel").fadeIn("fast");
+            $(".panel").addClass("showli").fadeIn("fast");
         }
     });
+//    点击订单状态分类显示相应的订单
+    
+    
     $(".menu-item>li").click(function () {
         $(this).addClass("active").siblings().removeClass("active");
         var select=$(this).attr("data-select");console.log(select);
         $(".show-area>div[data-state="+select+"]") .show().siblings().hide();
     });
+//点击左侧菜单显示相应的div
+  
     $(".Address-table").on("click",".Address-delect",function () {
         $(this).parents("tr").remove();
     });
+//删除地址
+    
     $(".Address-addsubmit").click(function () {
         var address=$("#addressDetail").val();
         var postcode=$("#addressPostcode").val();
@@ -36,19 +44,26 @@ $(document).ready(function () {
             return false;
         }
     });
+//添加地址细节
+    
     $(".Address-add").click(function () {
         $(".Address-addsubmit").show();
         $(".Address-mod").hide();
     });
+//添加地址按钮的点击事件
+    
     $(".search-button").click(function () {
         $("div.panel").hide();
         var orderid=$("#search").val();
        $(".Order-id:contains("+orderid+")").parents(".panel").show();
     });
+//订单号查询
+    
     $(".panel .panel-heading").on("click","i",function () {
         $(this).parents(".panel").remove();
     });
-
+//删除订单
+    
     $(".Address-table tbody").on("click",".Address-modify",function () {
         $modify=$(this);
         var name=$(this).parents("td").siblings(".addressName").text();
@@ -64,6 +79,8 @@ $(document).ready(function () {
         $("#addressPostcode").val(postcode);
         $("#addressPhone").val(phone);
 });
+//地址修改
+    
     $(".Address-mod").click(function () {
         $modify.parents("td").siblings(".addressName").text($("#addressName").val());
         $modify.parents("td").siblings(".addressDetail").text($("#addressDetail").val());
@@ -72,6 +89,52 @@ $(document).ready(function () {
         $("#addressName").val("");
         $("#addressDetail").val("");
         $("#addressPhone").val("");
-        $("#addressPostcode").val("")
+        $("#addressPostcode").val("");
+    });
+//地址修改
+    
+    $(".panel").each(function(){
+    	var page=$(this).attr("data-page");
+    	var pageNumber=Math.ceil((parseFloat(page)+1.0)/5.0);
+    	$(this).attr("data-pagenumber",pageNumber);
+    	console.log(pageNumber);	
+    });
+    console.log($(".panel:last").attr("data-pagenumber"));
+    var MaxPage=$(".panel:last").attr("data-pagenumber");
+    var j=1;
+    for(j;j<=MaxPage;j++){
+    	var option='<option value=\"'+j+'\">第'+j+'页</option>';
+    	$("#PageNumber").append(option);	
+    } 
+    console.log($("#PageNumber").val());
+    function check(){
+    $(".panel").each(function(){
+    	if($(this).attr("data-pagenumber")==$("#PageNumber").val()){
+    		$(this).show();
+    	}
+    	else{
+    		$(this).hide();
+    	}
+    });
+    }
+    $("#pre").click(function(){
+    	if($("#PageNumber").val()>1){
+    	$("#PageNumber").val($("#PageNumber").val()-1);
+    	console.log($("#PageNumber").val());
+    	check();
+    	}
+    	else{
+    		alert("已经是第一页了");
+    	}
+    });
+    $("#last").click(function(){
+    	if($("#PageNumber").val()<MaxPage){
+    	$("#PageNumber").val($("#PageNumber").val()+1);
+    	console.log($("#PageNumber").val());
+    	check();
+    	}
+    	else{
+    		alert("已经是最后一页了");
+    	}
     });
 });
