63350840ceb6ff7342ba3be11623dc62ac40d287
Alex <1515186599@qq.com>
2018-08-04 01:00:52 +0800

    random

--- src/main/java/service/welcomeservice/WelcomeserviceImpl.java
+++ src/main/java/service/welcomeservice/WelcomeserviceImpl.java
@@ -1,6 +1,7 @@
 package service.welcomeservice;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -16,27 +17,32 @@ import bean.middle.indexbean;
 
 @Service
 public class WelcomeserviceImpl implements Welcomeservice {
-    
+
 	@Autowired
 	GoodsMainTypeMapper goodsmaintypemapper;
 	@Autowired
 	GoodsSecondaryTypeMapper goodssecondarytypemapper;
+
 	@Override
 	public List<indexbean> initIndex() {
-		List<indexbean> result=new ArrayList<>();
-		List<GoodsMainType> goodsmaintypes=goodsmaintypemapper.findAllGoodsMainTypes();
-		for(GoodsMainType goodsmaintype:goodsmaintypes){
-			indexbean bean=new indexbean();
+		List<indexbean> result = new ArrayList<>();
+		List<GoodsMainType> goodsmaintypes = goodsmaintypemapper.findAllGoodsMainTypes();
+		for (GoodsMainType goodsmaintype : goodsmaintypes) {
+			indexbean bean = new indexbean();
 			bean.setMainType(goodsmaintype.getGoodsMainType());
 			bean.setMainTypeId(goodsmaintype.getGoodsMainTypeID());
 			bean.setMainTypePicture(goodsmaintype.getPicture().getPicturePath());
 			bean.setSecondaryTypes(goodsmaintype.getGoodsSecondaryTypeList());
-			List<Goods> goods=goodsmaintype.getGoodsList();
-			Goods good[]=new Goods[4];
-			Random random=new Random();
-			for(int i=0;i<4;i++){
-			    int index=random.nextInt(goods.size());
-			    good[i]=goods.get(index);
+			List<Goods> goods = goodsmaintype.getGoodsList();
+			Goods good[] = new Goods[4];
+			Random random = new Random();
+			HashSet<Integer> pool = new HashSet<>();
+			int index;
+			for (int i = 0; i < 4; i++) {
+				do {
+					index = random.nextInt(goods.size());
+				} while (!pool.add(index));
+				good[i] = goods.get(index);
 			}
 			bean.setGoods(good);
 			result.add(bean);
