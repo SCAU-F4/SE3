3c8804ba33019b4ec72298a5dc8f5e86a7406c87
Alex <1515186599@qq.com>
2018-08-04 22:22:35 +0800

    removefromcart finish

--- src/main/java/service/goodservice/GoodserviceImpl.java
+++ src/main/java/service/goodservice/GoodserviceImpl.java
@@ -12,12 +12,12 @@ import Mapper.GoodsMapper;
 import Mapper.GoodsSecondaryTypeMapper;
 import bean.Cart;
 import bean.CartDetail;
+import bean.Customer;
 import bean.Goods;
-import bean.GoodsSecondaryType;
 import bean.middle.categorybean;
 
 @Service
-public class GoodserviceImpl implements Goodservice{
+public class GoodserviceImpl implements Goodservice {
 	@Autowired
 	GoodsMapper goodsmapper;
 	@Autowired
@@ -28,34 +28,109 @@ public class GoodserviceImpl implements Goodservice{
 	CartMapper cartmapper;
 	@Autowired
 	CartDetailMapper cartdetailmapper;
+
 	@Override
 	public categorybean getcategory(int categoryID) {
-		categorybean result=new categorybean();
+		categorybean result = new categorybean();
 		result.setCategoryType(goodsmainmapper.findByGoodsMainTypeID(categoryID).getGoodsMainType());
 		result.setGoodssecondarytypelist(goodssecondarytypemapper.findBygoodsMainTypeID(categoryID));
 		return result;
 	}
+
 	@Override
 	public List<Goods> getgood(int goodsID) {
 		return goodsmapper.findBygoodsID(goodsID);
 	}
+
 	@Override
 	public List<Goods> searchItems(String goodsName) {
 		return goodsmapper.findBygoodsName(goodsName);
 	}
+
 	@Override
-	public String add2Cart(int goodsID, String goodsSpecify, int goodsCount, double goodsPrice, int cartID) {
-		String result="";
-		Goods good=new Goods(goodsID, null, 0, goodsSpecify, null, goodsPrice, 0, 0, null, 0, 0, null, null);
-		double totalPrice=goodsCount*goodsPrice;
-		CartDetail cartDetail=new CartDetail(cartID, good, goodsCount, totalPrice);
-		int num=cartdetailmapper.insert(cartDetail);
-		if(num==0){
-			result+="加入购物车失败";
-			return result;
+	public String add2Cart(int goodsID, String goodsSpecify, int goodsCount, Customer customer) {
+		String result = "";
+		Goods good = goodsmapper.findBygoodsIDAndgoodsSpecify(goodsID, goodsSpecify);
+		double totalPrice = goodsCount * good.getGoodsPrice();
+		CartDetail cartDetail = new CartDetail(customer.getCart().getCartID(), good, goodsCount, totalPrice);// new
+		int sum = cartdetailmapper.isCartDetailExist(cartDetail);
+		double tprice = customer.getCart().getTotalPrice();
+		tprice += totalPrice;// 购物车总价
+		if (sum == 0) {
+			sum = cartdetailmapper.insert(cartDetail);
+			if (sum == 0) {
+				result += "加入购物车失败";
+				return result;
+			}
+			customer.getCart().getCartDetailList().add(cartDetail);
+		} else {
+			cartDetail = cartdetailmapper.findBycartIDAndgoodsIDAndgoodsSpecify(customer.getCart().getCartID(), goodsID,
+					goodsSpecify);// 旧的
+			cartDetail.setGoodsCount(goodsCount + cartDetail.getGoodsCount());
+			cartDetail.setTotalPrice(totalPrice + cartDetail.getTotalPrice());
+			sum = cartdetailmapper.update(cartDetail);
+			if (sum == 0) {
+				result += "加入购物车失败";
+				return result;
+			}
+			for (CartDetail detail : customer.getCart().getCartDetailList()) {
+				if (detail.getGood().getGoodsID() == cartDetail.getGood().getGoodsID()
+						&& detail.getGood().getGoodsSpecify().equals(cartDetail.getGood().getGoodsSpecify())) {
+					customer.getCart().getCartDetailList().remove(detail);
+					customer.getCart().getCartDetailList().add(cartDetail);
+					break;
+				}
+			}
 		}
-		totalPrice+=cartmapper.findtotalPriceByCartID(cartID);
-		cartmapper.updatetotalPriceBycartID(cartID, totalPrice);
+		customer.getCart().setTotalPrice(tprice);
+		cartmapper.updatetotalPriceBycartID(customer.getCart().getCartID(), tprice);
+		return result;
+	}
+
+	@Override
+	public String removefromCart(int goodsID, String goodsSpecify, int goodsCount, Customer customer) {
+		String result = "";
+		Goods good = goodsmapper.findBygoodsIDAndgoodsSpecify(goodsID, goodsSpecify);
+		double totalPrice = goodsCount * good.getGoodsPrice();
+		CartDetail cartDetail = cartdetailmapper.findBycartIDAndgoodsIDAndgoodsSpecify(customer.getCart().getCartID(),
+				goodsID, goodsSpecify);
+		int currentCount = cartDetail.getGoodsCount();
+		currentCount += goodsCount;
+		if (currentCount != 0) {
+			cartDetail.setGoodsCount(currentCount);
+			cartDetail.setTotalPrice(cartDetail.getTotalPrice() + totalPrice);
+			int sum = cartdetailmapper.update(cartDetail);
+			if (sum == 0) {
+				result = "移除失败";
+				return result;
+			}
+			cartmapper.updatetotalPriceBycartID(customer.getCart().getCartID(),
+					customer.getCart().getTotalPrice() + totalPrice);
+			for (CartDetail detail : customer.getCart().getCartDetailList()) {
+				if (detail.getGood().getGoodsID() == cartDetail.getGood().getGoodsID()
+						&& detail.getGood().getGoodsSpecify().equals(cartDetail.getGood().getGoodsSpecify())) {
+					detail = cartDetail;
+					break;
+				}
+			}
+			customer.getCart().setTotalPrice(customer.getCart().getTotalPrice() + totalPrice);
+		} else {
+			int sum = cartdetailmapper.delete(cartDetail);
+			if (sum == 0) {
+				result = "移除失败";
+				return result;
+			}
+			cartmapper.updatetotalPriceBycartID(customer.getCart().getCartID(),
+					customer.getCart().getTotalPrice() + totalPrice);
+			for (CartDetail detail : customer.getCart().getCartDetailList()) {
+				if (detail.getGood().getGoodsID() == cartDetail.getGood().getGoodsID()
+						&& detail.getGood().getGoodsSpecify().equals(cartDetail.getGood().getGoodsSpecify())) {
+					customer.getCart().getCartDetailList().remove(detail);
+					break;
+				}
+			}
+			customer.getCart().setTotalPrice(customer.getCart().getTotalPrice() + totalPrice);
+		}
 		return result;
 	}
 }
